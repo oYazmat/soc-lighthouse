3d99f752978de990db791fb5e43558513e88e2c7
Yazmat <j.yazmat@gmail.com>
2025-08-27 20:22:02 -0400

    Add basename to react-router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,5 +5,5 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   prerender: true,
-  basename: "/",
+  basename: "/soc-lighthouse/",
 } satisfies Config;
