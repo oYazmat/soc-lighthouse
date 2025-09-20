139d035676af9fd0a2994d1f925e34c76c60ced0
Yazmat <j.yazmat@gmail.com>
2025-09-20 11:28:25 -0400

    WIP Events Calendar

---
+++ app/routes/events.tsx
@@ -0,0 +1,8 @@
+export default function Events() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Event Calendar</h1>
+      <p>Here you can put your event schedule or calendar UI.</p>
+    </div>
+  );
+}
